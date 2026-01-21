52547109e0ef756626abec5aa0331aa2e965e8a0
nyniraula <nyniraula@gmail.com>
2026-01-21 12:20:42 +0000

    ReviewComp

--- src/Pages/Product/Review.jsx
+++ src/Pages/Product/Review.jsx
@@ -1,33 +1,29 @@
 import React from "react";
 import { Star, CircleUserRound, Circle } from "lucide-react";
+import { Rating } from "@smastrom/react-rating";
+
+function RatingStar() {
+  return <Rating style={{ maxWidth: 70 }} value={4.5} readOnly />;
+}
 
 const ReviewComp = () => {
   return (
-    <div className=" w-full h-fit  flex flex-col justify-center items-start px-2 border-gray-400/30 border mb-2 gap-1.5 py-2 shrink-0">
-      <div className="flex  w-full font-mont justify-between">
-        <div className="flex justify-center items-center gap-2 ">
-          <CircleUserRound size={28} color="#525252" />
-          <div className="flex flex-col">
-            <span className="text-sm font-semibold">John Doe</span>
-          </div>
-        </div>
-        <div className="flex justify-center items-center">
-          <Star size={14} fill="#ffd700" color="#ffd700" />
-          <Star size={14} fill="#ffd700" color="#ffd700" />
-          <Star size={14} fill="#ffd700" color="#ffd700" />
-          <Star size={14} fill="#ffd700" color="#ffd700" />
-          <Star size={14} fill="#ffd700" color="#ffd700" />
-        </div>
-      </div>
-
+    <div className=" w-full h-fit  flex flex-col justify-center items-start px-2 bg-white border-gray-400/30 border mb-2 gap-1 py-2 shrink-0">
       <span className="font-mont flex justify-center items-center font-semibold">
-        Lorem, ipsum dolor.
+        Title
       </span>
 
       <span className="font-mont flex justify-center items-center text-[12px]">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima culpa
         nisi tempore error deleniti? Tempora ipsum a delectus ea numquam
       </span>
+
+      <div className="flex  w-full font-mont justify-between px-2 border-gray-400/30 border text-white bg-black  ">
+        <div className="flex justify-center items-center gap-2 ">
+          <span className="text-sm font-semibold">John Doe</span>
+        </div>
+        <RatingStar />
+      </div>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import ContextApi from './Context/ContextApi.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import ContextApi from "./Context/ContextApi.jsx";
+import "@smastrom/react-rating/style.css";
 
-createRoot(document.getElementById('root')).render(
-    <ContextApi>
-        <App />
-    </ContextApi>
-
-
-)
+createRoot(document.getElementById("root")).render(
+  <ContextApi>
+    <App />
+  </ContextApi>,
+);
